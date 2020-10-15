814403582f35a281ba0f7077d39183271b999b88
thomashewett <thomashewett@hotmail.com>
2020-10-15 18:50:01 +1100

    Core Game Logic Complete

--- life.js
+++ life.js
@@ -12,11 +12,14 @@ const CELL_ROW_COUNT = 20;
 const GRID_STROKE_WIDTH = 1;
 const GRID_COLOUR = "#0095DD";
 const BACKGROUND_COLOUR = "white";
+const NEXT_ALIVE_COLOUR = "yellow";
+const NEXT_DEAD_COLOUR = "grey";
 
 //cell variables
 const CELL_HEIGHT = canvasHeight / CELL_ROW_COUNT;
 const CELL_WIDTH = canvasWidth / CELL_COLUMN_COUNT;
 const CELL_COLOUR = "#0095DD";
+const NEIGHBOUR_COUNT_COLOUR = "black";
 let cells = [];
 for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
     cells[c] = [];
@@ -26,6 +29,7 @@ for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
         cells[c][r] = {
             x: cellX,
             y: cellY,
+            neighbours: 0,
             alive: false,
             aliveNext: false,
         }
@@ -36,6 +40,8 @@ for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
 //game variables
 let playing = false;
 let iterateGeneration = false;
+let displayAliveDeadNext = false;
+let displayNeighbours = false;
 const GENERATION_TIME = 200;
 
 /* ---------------------------- event listeners --------------------------- */
@@ -43,6 +49,8 @@ const GENERATION_TIME = 200;
 canvas.addEventListener('mousedown', mouseDownHandler);
 document.querySelector('.start').addEventListener('click', startButtonHandler);
 document.querySelector('.next').addEventListener('click', nextButtonHandler);
+document.querySelector('.rst').addEventListener('click', resetButtonHandler);
+document.querySelector('.neighbours').addEventListener('click', neighbourButtonHandler);
 
 /* ------------------------------- Handlers ------------------------------- */
 
@@ -55,23 +63,36 @@ function mouseDownHandler(e) {
     //toggle its alive attribute and redraw
     cells[gridXSelect][gridYSelect].alive = !cells[gridXSelect][gridYSelect].alive;
     cells[gridXSelect][gridYSelect].aliveNext = false;
-    ctx.beginPath();
-    ctx.rect(cells[gridXSelect][gridYSelect].x + GRID_STROKE_WIDTH, cells[gridXSelect][gridYSelect].y + GRID_STROKE_WIDTH, CELL_WIDTH - 2 * GRID_STROKE_WIDTH, CELL_HEIGHT - 2 * GRID_STROKE_WIDTH);
-    if (cells[gridXSelect][gridYSelect].alive === true)
-        ctx.fillStyle = CELL_COLOUR;
-    else
-        ctx.fillStyle = BACKGROUND_COLOUR;
-    ctx.fill();
-    ctx.closePath();
+    calculateNeighbourCount();
+    draw();
 }
 
 function startButtonHandler(e) {
     playing = !playing;
-    console.log(playing);
+    if (playing)
+        document.querySelector('.start').innerHTML = "Stop";
+    else
+        document.querySelector('.start').innerHTML = "Start";
 }
 
-function nextButtonHandler(e){
+function nextButtonHandler(e) {
     iterateGeneration = true;
+}
+
+function resetButtonHandler(e) {
+    for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
+        for (let r = 0; r < CELL_ROW_COUNT; r++) {
+            cells[c][r].alive = false;
+            cells[c][r].aliveNext = false;
+            cells[c][r].neighbours = 0;
+        }
+    }
+    draw();
+}
+
+function neighbourButtonHandler(e) {
+    displayNeighbours = !displayNeighbours;
+    draw();
 }
 
 /* ----------------------------- Draw Functions ----------------------------- */
@@ -79,14 +100,16 @@ function nextButtonHandler(e){
 function drawCells() {
     for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
         for (let r = 0; r < CELL_ROW_COUNT; r++) {
-            if (cells[c][r].aliveNext == true) {
-                ctx.beginPath();
-                ctx.rect(cells[c][r].x, cells[c][r].y, CELL_WIDTH, CELL_HEIGHT);
-                ctx.fillStyle = CELL_COLOUR;
-                ctx.fill();
-                ctx.closePath();
-                alive: true;
-                aliveNext: false;
+            if (displayAliveDeadNext) {
+
+            } else {
+                if (cells[c][r].alive == true) {
+                    ctx.beginPath();
+                    ctx.rect(cells[c][r].x, cells[c][r].y, CELL_WIDTH, CELL_HEIGHT);
+                    ctx.fillStyle = CELL_COLOUR;
+                    ctx.fill();
+                    ctx.closePath();
+                }
             }
         }
     }
@@ -114,17 +137,27 @@ function draw() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     drawCells();
     drawGrid();
+    if (displayNeighbours)
+        displayNeighbourCount(); //purely for testing
 }
 
 /* --------------------------------- Testing -------------------------------- */
+function displayNeighbourCount() {
+    ctx.font = "20px Arial";
+    ctx.fillStyle = NEIGHBOUR_COUNT_COLOUR;
 
+    for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
+        for (let r = 0; r < CELL_ROW_COUNT; r++) {
+            ctx.fillText(cells[c][r].neighbours, c * CELL_WIDTH, r * CELL_HEIGHT + CELL_HEIGHT);
+        }
+    }
+}
 
 /* -------------------------------- game functions ------------------------------- */
-function nextGeneration() {
+function calculateNeighbourCount() {
     for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
         for (let r = 0; r < CELL_ROW_COUNT; r++) {
             let neighbourCount = 0;
-
             //check surrounding and count alive cells
             for (let XOffset = -1; XOffset < 2; XOffset++) {
                 for (let YOffset = -1; YOffset < 2; YOffset++) {
@@ -138,18 +171,32 @@ function nextGeneration() {
                     }
                 }
             }
+            cells[c][r].neighbours = neighbourCount;
+        }
+    }
+}
 
+function nextGeneration() {
+    for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
+        for (let r = 0; r < CELL_ROW_COUNT; r++) {
             //determine if cell will live to next generation
-            if (neighbourCount < 2)
+            if (cells[c][r].neighbours < 2)
                 cells[c][r].aliveNext = false;
-            else if (neighbourCount == 2 && cells[c][r].alive == true)
+            else if (cells[c][r].neighbours == 2 && cells[c][r].alive == true)
                 cells[c][r].aliveNext = true;
-            else if(neighbourCount == 3)
+            else if (cells[c][r].neighbours == 3)
                 cells[c][r].aliveNext = true;
-            else if(neighbourCount > 3)
+            else if (cells[c][r].neighbours > 3)
                 cells[c][r].aliveNext = false;
         }
     }
+    for (let c = 0; c < CELL_COLUMN_COUNT; c++) {
+        for (let r = 0; r < CELL_ROW_COUNT; r++) {
+            cells[c][r].alive = cells[c][r].aliveNext
+            cells[c][r].aliveNext = false;
+        }
+    }
+    calculateNeighbourCount();
 }
 
 function gameLoop() {
